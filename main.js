140c9df2d164217582e9d650ba2549668305b5b0
professura <professura18@gmail.com>
2020-02-26 10:04:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-$("#switch").click(function () {
-    if ($("#fullpage").hasClass("night")) {
-        $("#fullpage").removeClass("night");
-        $("#switch").removeClass("switched");
-    }
-    else {
-        $("#fullpage").addClass("night");
-        $("#switch").addClass("switched");
 
-    }
-});
